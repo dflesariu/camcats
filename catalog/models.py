aeeb58ac611b20436cabadd3a0fdfb177d5f926f
dflesariu <flesariudragos@gmail.com>
2025-03-17 23:06:45 +0000

    changes to cat model

--- catalog/models.py
+++ catalog/models.py
@@ -36,19 +36,17 @@ class Cat(models.Model):
 
     description = models.TextField(
         max_length=1000, help_text="Enter a brief description of the cat")
-    nickname = models.CharField('Nickname', max_length=50,
+    nickname = models.CharField('Nickname', max_length=150,
                             unique=False,
                             help_text='Made up nickname for the cat when we do not know its actual name')
 
-    # ManyToManyField used because genre can contain many books. Books can cover many genres. Not the case for cats usually
-    # Genre class has already been defined so we can specify the object above.
-    breed = models.ManyToManyField(
-        Breed, help_text="Select a breed")
+    # Foreign Key used because a cat can only have one breed, but breeds can have multiple cats.
+    breed = models.ForeignKey('Breed', on_delete=models.RESTRICT, null=True)
 
     def __str__(self):
         """String for representing the Model object."""
         return self.title
 
     def get_absolute_url(self):
-        """Returns the URL to access a detail record for this book."""
+        """Returns the URL to access a detail record for this cat."""
         return reverse('cat-detail', args=[str(self.id)])
